f30d50f0d99565d9f94bd29329cd3e09339f0064
LEMON8188 <muhammadnabijonov4@gmail.com>
2025-08-19 23:53:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let ism = prompt("ism yoz")
+let harf = prompt("harf kirit")
+
+if(ism.toLowerCase().includes(harf.toLowerCase() )) {
+    alert("ismda bu harf tashkil etgan")
+}
+else{
+    alert(ism + " ismida yogu")
+}
